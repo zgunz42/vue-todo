d73a71fbe786519336add50aa3d218abfb9a4db0
Adi Gunawan <adi_gunawan@live.com>
2020-06-12 16:17:07 +0800

    template file

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 new Vue({
   store,
